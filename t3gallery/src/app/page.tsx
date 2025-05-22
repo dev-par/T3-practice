a3e969020cb168d2d07b3cb75589cb53a9765b07
dev-par <146295367+dev-par@users.noreply.github.com>
2025-05-22 13:15:02 +0200

    scaffold UI

--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,8 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://r3uvndu328.ufs.sh/f/xTpNYJOpXr71L7ME1YJcKBkZtHY0sXfVmiOAQu83q6jr4UoN",
+  "https://r3uvndu328.ufs.sh/f/xTpNYJOpXr71dnT7aLbVw5gcUK4ZmEkdyGjMACQxL6iJFpYs",
+  "https://r3uvndu328.ufs.sh/f/xTpNYJOpXr71tJUdt36P1we32cYf78xn4OTjXAb9iLoplDJv",
+  "https://r3uvndu328.ufs.sh/f/xTpNYJOpXr71ogLkzMmAvrQ5zXVO39SWymEiGN04n61MLhJj"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
      Hello (gallery in progress)
     </main>
   );
