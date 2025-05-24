0992dfe8dcd0978f98e18f88967a72e339e29c3a
dev-par <146295367+dev-par@users.noreply.github.com>
2025-05-24 21:47:21 +0200

    configured auth settings

--- t3gallery/src/app/page.tsx
+++ t3gallery/src/app/page.tsx
@@ -1,18 +1,18 @@
+import { SignedIn } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
+import { SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
 
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
-    <main className="">
       <div className="flex flex-wrap gap-4">
-
         {[...images, ...images].map((image, index) => (
           <div key={image.id + "-" + index} className="flex w-48 flex-col">
             <img src={image.url} />
@@ -20,7 +20,20 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
-     Hello (gallery in progress)
+  );
+}
+
+export default async function HomePage() {
+
+  return (
+    <main className="">
+
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center">Please sign in above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
